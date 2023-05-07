1d2e028e109740ac8c9c0e662ce1b43fd7c91589
p32 <ostik19791108@gmail.com>
2023-05-07 14:49:10 +0300

    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

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
-print('')
+# dict_1 = {
+#     'Nick':{
+#         'phone':'2342334324234',
+#         'insta':'@fssd'
+#     },
+#     'Anna':{
+#         'phone':'123125324513',
+#         'insta':'@21321ls'
+#     },
+#     'Rick':{
+#         'phone':'021843094032',
+#         'insta':'@;s;doif'
+#     }
+# }
+#
+#
+# user = input('Enter name: ')
+# userin = input('Enter 2 : ')
+#
+# print(dict_1[user][userin])
+
+dict_1 = {
+    'Nick':{
+        'phone':'2342334324234',
+        'insta':'@fssd'
+    },
+    'Anna':{
+        'phone':'123125324513',
+        'insta':'@21321ls'
+    },
+    'Rick':{
+        'phone':'021843094032',
+        'insta':'@;s;doif'
+    }
+}
+
+user = input('Enter name: ')
+
+for x in dict_1:
+    if x == user:
+        print('True')
